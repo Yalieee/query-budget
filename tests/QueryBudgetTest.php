66c7fa8db99ab13297aa0d9100b63a8cca93b786
ron.liu <ron.liu@linecorp.com>
2019-06-26 16:06:51 +0800

    Add all testcase

--- tests/QueryBudgetTest.php
+++ tests/QueryBudgetTest.php
@@ -1,15 +1,47 @@
 <?php
 
+
 namespace App;
 
-use App\QueryBudget;
 use PHPUnit\Framework\TestCase;
 
 class QueryBudgetTest extends TestCase
 {
+    private $sut;
+
+    public function setUp()
+    {
+        $this->sut = new QueryBudget($findAllBudgets);
+        parent::setUp();
+    }
+
     public function testQueryOneMouth()
     {
-        $budget = new QueryBudget();
-        $this->assertEquals(31, $budget->query('2019-01-01', '2019-01-31'));
+        $this->assertEquals(31, $this->sut->query('2019-01-01', '2019-01-31'));
+    }
+
+    public function testOneMonth()
+    {
+        $this->assertEquals(31, $sut->query('2019-01-01', '2019-01-31'));
+    }
+
+    public function testOneDay()
+    {
+        $this->assertEquals(31, $sut->query('2019-01-01', '2019-01-31'));
+    }
+
+    public function testCrossMonth()
+    {
+        $this->assertEquals(31, $sut->query('2019-01-01', '2019-01-31'));
+    }
+
+    public function testHaveNoBudgets()
+    {
+        $this->assertEquals(31, $sut->query('2019-01-01', '2019-01-31'));
+    }
+
+    public function testStartDateGreaterThanEndDate()
+    {
+        $this->assertEquals(31, $sut->query('2019-01-01', '2019-01-31'));
     }
 }
